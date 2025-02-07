40466910e0bf73c2df5bc8f8d918bdbffa9331fe
Nakah23 <yunak@hotmail.es>
2025-02-07 23:11:57 +0100

    Update config.lua Nakah23
    Deprecated Config.BoomboxItem

--- config.lua
+++ config.lua
@@ -6,6 +6,6 @@ Config = {}
 
 Config.checkForUpdates = true -- Check for Updates?
 
-Config.BoomboxItem = 'boombox'
+--Config.BoomboxItem = 'boombox' --DEPRECATED? Item System....
 
 Config.InstructionNotification = false -- If you want a notification explaining to press E to drop boombox
